f790de9dcbeb08eea7410ca499268f3b90950b73
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-12-03 22:03:27 +0100

    better handling of predict in face alignment wrapper

--- loreal_poc/models/face_alignment.py
+++ loreal_poc/models/face_alignment.py
@@ -3,12 +3,20 @@ import numpy as np
 
 
 class FaceAlignmentModel:
-
     def __init__(self):
         self.model = FaceAlignment(LandmarksType.TWO_D, device="cpu", flip_input=False)
-        
-    def predict_image(self, image):
-        return np.array(self.model.get_landmarks(np.array(image)))[0]
 
-    def predict(self, ds):
-        return np.array([self.predict_image(ds.all_images[i]) for i in range(len(ds))])
+    def _predict_image(self, image):
+        return np.array(self.model.get_landmarks(np.array(image)))
+
+    def predict(self, ds, idx_range=None):
+        predictions = list()
+        idx_range = idx_range if idx_range is not None else range(len(ds))
+        for i in idx_range:
+            prediction = self._predict_image(ds.all_images[i])
+            if not prediction.shape:
+                prediction = np.empty((1, ds.n_landmarks, ds.n_dimensions))
+                prediction[:, :, :] = np.nan
+            predictions.append(prediction[0])
+
+        return np.array(predictions)
