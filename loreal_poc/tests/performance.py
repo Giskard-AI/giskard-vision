7356bb6730850ef0ca8b6e89829c4cfdd0f60e9b
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-12-03 18:29:55 +0100

    introduced np einsum for the metrics

--- loreal_poc/tests/performance.py
+++ loreal_poc/tests/performance.py
@@ -2,53 +2,45 @@ import numpy as np
 
 from loreal_poc.tests.base import TestResult
 
+LEFT_EYE_LEFT_LANDMARK = 36
+RIGHT_EYE_RIGHT_LANDMARK = 45
 
-def _calculate_med(points1, points2):
-    """
-    Mean Euclidean distances
-    :param points1:
-    :param points2:
-    :return:
-    """
-    if points1.shape != points2.shape:
-        raise ValueError("_calculate_MDE: arrays have different dimensions")
-    return np.linalg.norm(points1 - points2) / points1.shape[0]
-
-
-def _calculate_meds(model, dataset):
+def _get_predictions_and_marks(model, dataset):
     predictions = model.predict(dataset)
     marks = dataset.all_marks
-    return [_calculate_med(predictions[i], marks[i]) for i in range(len(dataset))]
+    if predictions.shape != marks.shape:
+        raise ValueError("_calculate_me: arrays have different dimensions.")
+    if len(predictions.shape) > 3 or len(marks.shape) > 3:
+        raise ValueError("_calculate_me: ME only implemented for 2D images.")
+    
+    return predictions, marks
 
+def _calculate_es(predictions, marks):
+    """
+    Euclidean distances
+    """
+    return np.sqrt(np.einsum('ijk->ij',(predictions-marks)**2))
 
-def _calculate_nmed(points1, points2):
+def _calculate_d_outers(marks):
+    return np.sqrt(np.einsum('ij->i',(marks[:,LEFT_EYE_LEFT_LANDMARK,:]-marks[:,RIGHT_EYE_RIGHT_LANDMARK,:])**2))
+
+def _calculate_nmes(predictions, marks):
     """
     Normalized Mean Euclidean distances
-    :param points1:
-    :param points2:
-    :return:
     """
-    d_outer = np.linalg.norm(points1[37 - 1] - points2[46 - 1])
-    return _calculate_med(points1, points2) / d_outer
-
-
-def _calculate_nmeds(model, dataset):
-    predictions = model.predict(dataset)
-    marks = dataset.all_marks
-    return [_calculate_nmeds(predictions[i], marks[i]) for i in range(len(dataset))]
-
-
-def _calculate_nmeds(model, dataset):
-    predictions = model.predict(dataset)
-    marks = dataset.all_marks
-    return [_calculate_nmed(predictions[i], marks[i]) for i in range(len(dataset))]
+    es = _calculate_es(predictions, marks)
+    mes = np.nanmean(es, axis=1)
+    d_outers = _calculate_d_outers(marks)
+    return mes / d_outers
 
 
 def test_med(model, dataset, threshold=1):
-    metric = np.mean(_calculate_meds(model, dataset))
-    return TestResult(name="Mean Euclidean Distance (MED)", metric=metric, passed=metric <= threshold)
+    predictions, marks = _get_predictions_and_marks(model, dataset)
+    metric = np.nanmean(_calculate_es(predictions, marks))
+    return TestResult(name="Mean Euclidean Distance (ME)", metric=metric, passed=metric <= threshold)
 
 
 def test_nmed(model, dataset, threshold=0.01):
-    metric = np.mean(_calculate_nmeds(model, dataset))
-    return TestResult(name="Normalized Mean Euclidean Distance (NMED)", metric=metric, passed=metric <= threshold)
+    predictions, marks = _get_predictions_and_marks(model, dataset)
+    metric = np.nanmean(_calculate_nmes(predictions, marks))
+    return TestResult(name="Normalized Mean Euclidean Distance (NME)", metric=metric, passed=metric <= threshold)
