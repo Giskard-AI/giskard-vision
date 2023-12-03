7e848933e452cddb6fb36f7ef7149d7fb1f21d14
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-12-03 18:28:48 +0100

    refactored base dataset

--- loreal_poc/datasets/base.py
+++ loreal_poc/datasets/base.py
@@ -1,43 +1,127 @@
-from typing import Union, Iterable
+from typing import Union
 from pathlib import Path
 import os
 from dataclasses import dataclass
-from PIL import Image
+import numpy as np
+from abc import ABC, abstractmethod
+from numpy.lib.mixins import NDArrayOperatorsMixin
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class FacialPart(NDArrayOperatorsMixin):
+    part: np.ndarray
+    
+    def __add__(self, o):
+        return FacialPart(np.concatenate((self.part, o.part)))
+    
+    def __array__(self):
+        return self.part
+    
+
+@dataclass(frozen=True)
+class FacialParts:
+    all: FacialPart = FacialPart(np.arange(0, 68))
+    contour: FacialPart = FacialPart(np.arange(0, 17))
+    left_eyebrow: FacialPart = FacialPart(np.arange(17, 22))
+    right_eyebrow: FacialPart = FacialPart(np.arange(22, 27))
+    nose: FacialPart = FacialPart(np.arange(27, 36))
+    left_eye: FacialPart = FacialPart(np.arange(35, 42))
+    right_eye: FacialPart = FacialPart(np.arange(42, 48))
+    mouth: FacialPart = FacialPart(np.arange(48, 68))
+    
+ALL = FacialParts.all
+CONTOUR = FacialParts.contour
+LEFT_EYEBROW = FacialParts.left_eyebrow
+RIGHT_EYEBROW = FacialParts.right_eyebrow
+NOSE = FacialParts.nose
+LEFT_EYE = FacialParts.left_eye
+RIGHT_EYE = FacialParts.right_eye
+MOUTH = FacialParts.mouth
 
 
-@dataclass
-class ImageWithMarks:
-    image_path: Path
-    marks_path: Path
-    image: Image
-    marks: Iterable
-
-
-class Base:
+class Base(ABC):
     def __init__(self,
-                 local_path: Union[str, Path],
-                 image_suffix: str = ".png", ) -> None:
-        self.local_path = Path(os.getcwd()) / local_path
-        if not os.path.exists(self.local_path):
-            raise ValueError(f"{self.__class__.__name__}: {self.local_path} does not exist")
-
-        self.all_files = os.listdir(self.local_path)
-        self.image_paths = [self.local_path / x for x in self.all_files if x.endswith(image_suffix)]
-
-        self.meta = dict()
-        self.meta.update({"num_samples": len(self.image_paths)})
-
-
-class LandmarkDataset(Base):
-    def __init__(self,
-                 local_path: Union[str, Path],
+                 images_dir_path: Union[str, Path],
+                 landmarks_dir_path: Union[str, Path],
                  image_suffix: str = ".png",
-                 marks_suffix: str = ".pts") -> None:
-        super().__init__(local_path=local_path, image_suffix=image_suffix)
-        self.marks_paths = [self.local_path / x for x in self.all_files if x.endswith(marks_suffix)]
-        if len(self.marks_paths) == 0:
-            raise ValueError(f"{self.__class__.__name__}: Landmarks with suffix {marks_suffix}"
-                             f" requested but no landmarks found in {local_path}.")
+                 marks_suffix: str = ".pts",
+                 n_landmarks: int = 68,
+                 n_dimensions: int = 2,
+                 ) -> None:
+        images_dir_path = self._get_absolute_local_path(images_dir_path)
+        landmarks_dir_path = self._get_absolute_local_path(landmarks_dir_path)
+
+        self.image_paths = self._get_all_paths_based_on_suffix(images_dir_path, image_suffix)
+        self.marks_paths = self._get_all_paths_based_on_suffix(landmarks_dir_path, marks_suffix)
+        self._all_marks = None
+        self._all_images = None
+        self.n_landmarks=n_landmarks
+        self.n_dimensions=n_dimensions
+
         if len(self.marks_paths) != len(self.image_paths):
             raise ValueError(f"{self.__class__.__name__}: Only {len(self.marks_paths)} found "
                              f"for {len(self.marks_paths)} of the images.")
+            
+        self.meta = dict()
+        self.meta.update({"num_samples": len(self)})
+        
+    def _get_absolute_local_path(self, local_path: Union[str, Path]):
+        local_path = Path(os.getcwd()) / local_path
+        if not os.path.exists(local_path):
+            raise ValueError(f"{self.__class__.__name__}: {local_path} does not exist")
+        return local_path
+    
+    @classmethod
+    def _get_all_paths_based_on_suffix(cls, dir_path: Union[str, Path], suffix: str):
+        all_paths = os.listdir(dir_path)
+        all_paths_with_suffix = sorted([dir_path / x for x in all_paths if x.endswith(suffix)])
+        if len(all_paths_with_suffix) == 0:
+            raise ValueError(f"{cls.__class__.__name__}: Landmarks with suffix {suffix}"
+                             f" requested but no landmarks found in {dir_path}.")
+        return all_paths_with_suffix
+        
+        
+    def __len__(self):
+        return len(self.image_paths)
+    
+    
+    @classmethod
+    @abstractmethod
+    def load_marks_from_file(cls, mark_file: Path):
+        ...
+        
+    @classmethod
+    @abstractmethod
+    def load_image_from_file(cls, image_file: Path):
+        ...
+    
+    @property
+    def all_marks(self):
+        if self._all_marks is None:
+            all_marks = np.empty((len(self), self.n_landmarks, self.n_dimensions))
+            for i, marks_path in enumerate(self.marks_paths):
+                _marks = self.load_marks_from_file(marks_path)
+                if _marks.shape[0] != self.n_landmarks:
+                    raise ValueError(f"{self.__class__} is only defined for {self.n_landmarks} landmarks.")
+                if _marks.shape[1] != self.n_dimensions:
+                    raise ValueError(f"{self.__class__} is only defined for {self.n_dimensions} dimensions.")
+                all_marks[i,:,:] = _marks
+            self._all_marks = all_marks
+        return self._all_marks
+                
+    @property
+    def all_images(self):
+        if self._all_images is None:
+            all_images = list()
+            for i, image_path in enumerate(self.image_paths):
+                _image = self.load_image_from_file(image_path)
+                all_images.append(_image)
+            self._all_images = all_images
+        return self._all_images    
+    
+    def all_marks_for(self, part: FacialPart, exclude=False):
+        idx = ~np.isin(ALL, part) if not exclude else np.isin(ALL, part)
+        part_landmarks = self.all_marks.copy()
+        part_landmarks[:, idx] = np.nan
+        return part_landmarks
+
