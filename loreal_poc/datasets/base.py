485ee1f6b0c82586aa33b33d849140091a393b7e
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-12-04 13:53:04 +0100

    additional relevant facial parts added

--- loreal_poc/datasets/base.py
+++ loreal_poc/datasets/base.py
@@ -18,26 +18,42 @@ class FacialPart(NDArrayOperatorsMixin):
         return self.part
 
 
+# see https://ibug.doc.ic.ac.uk/resources/300-W/ for definitions
+_entire = np.arange(0, 68)
+_contour = np.arange(0, 17)
+__left_contour = np.arange(0, 9)
+__right_contour = np.arange(10, 18)
+_left_eyebrow = np.arange(17, 22)
+_right_eyebrow = np.arange(22, 27)
+_nose = np.arange(27, 36)
+__left_nose = np.array([31, 32])
+__right_nose = np.array([34, 35])
+_left_eye = np.arange(36, 42)
+_right_eye = np.arange(42, 48)
+_mouth = np.arange(48, 68)
+__left_mouth = np.array([50, 49, 61, 48, 60, 67, 59, 58])
+__right_mouth = np.array([52, 53, 63, 64, 54, 65, 55, 56])
+_bottom_half = np.concatenate([np.arange(3, 15), _mouth])
+_upper_half = np.setdiff1d(_entire, _bottom_half, assume_unique=True)
+__center_axis = np.array([27, 28, 29, 30, 33, 51, 62, 66, 57, 8])
+_left_half = np.concatenate([__left_contour, _left_eyebrow, _left_eye, __left_nose, __left_mouth, __center_axis])
+_right_half = np.concatenate([np.setdiff1d(_entire, _left_half, assume_unique=True), __center_axis])
+
+
 @dataclass(frozen=True)
 class FacialParts:
-    entire: FacialPart = FacialPart(np.arange(0, 68))
-    contour: FacialPart = FacialPart(np.arange(0, 17))
-    left_eyebrow: FacialPart = FacialPart(np.arange(17, 22))
-    right_eyebrow: FacialPart = FacialPart(np.arange(22, 27))
-    nose: FacialPart = FacialPart(np.arange(27, 36))
-    left_eye: FacialPart = FacialPart(np.arange(36, 42))
-    right_eye: FacialPart = FacialPart(np.arange(42, 48))
-    mouth: FacialPart = FacialPart(np.arange(48, 68))
-
-
-ENTIRE = FacialParts.entire
-CONTOUR = FacialParts.contour
-LEFT_EYEBROW = FacialParts.left_eyebrow
-RIGHT_EYEBROW = FacialParts.right_eyebrow
-NOSE = FacialParts.nose
-LEFT_EYE = FacialParts.left_eye
-RIGHT_EYE = FacialParts.right_eye
-MOUTH = FacialParts.mouth
+    entire: FacialPart = FacialPart(_entire)
+    contour: FacialPart = FacialPart(_contour)
+    left_eyebrow: FacialPart = FacialPart(_left_eyebrow)
+    right_eyebrow: FacialPart = FacialPart(_right_eyebrow)
+    nose: FacialPart = FacialPart(_nose)
+    left_eye: FacialPart = FacialPart(_left_eye)
+    right_eye: FacialPart = FacialPart(_right_eye)
+    mouth: FacialPart = FacialPart(_mouth)
+    bottom_half: FacialPart = FacialPart(_bottom_half)
+    upper_half: FacialPart = FacialPart(_upper_half)
+    left_half: FacialPart = FacialPart(_left_half)
+    right_half: FacialPart = FacialPart(_right_half)
 
 
 class Base(ABC):
@@ -124,7 +140,7 @@ class Base(ABC):
         return self._all_images
 
     def all_marks_for(self, part: FacialPart, exclude=False):
-        idx = ~np.isin(ENTIRE, part) if not exclude else np.isin(ENTIRE, part)
+        idx = ~np.isin(FacialParts.entire, part) if not exclude else np.isin(FacialParts.entire, part)
         part_landmarks = self.all_marks.copy()
         part_landmarks[:, idx] = np.nan
         return part_landmarks
