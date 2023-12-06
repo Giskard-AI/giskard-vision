c215500477d27e14362d74b4f8160b3674ed1a71
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-12-07 00:24:02 +0100

    proper support of the two image types

--- loreal_poc/datasets/dataset_300W.py
+++ loreal_poc/datasets/dataset_300W.py
@@ -1,10 +1,35 @@
 from pathlib import Path
 from typing import Union
-from PIL import Image
+from PIL.Image import Image
+import cv2
 import numpy as np
 from time import time
 
 from .base import DatasetBase, FacialPart, FacialParts
+
+
+def _load_image_using_PIL(image_file: Path) -> Image:
+    """Loads images as PIL Image
+
+    Args:
+        image_file (Path): path to image file
+
+    Returns:
+        Image: PIL image
+    """
+    return Image.open(image_file).convert("RGB")
+
+
+def _load_image_usin_openCV(image_file: Path) -> np.ndarray:
+    """Loads images as np.array using opencV
+
+    Args:
+        image_file (Path): path to image file
+
+    Returns:
+        np.ndarray: numpy array image
+    """
+    return cv2.imread(str(image_file))
 
 
 class Dataset300W(DatasetBase):
@@ -45,8 +70,19 @@ class Dataset300W(DatasetBase):
         return marks
 
     @classmethod
-    def load_image_from_file(cls, image_file: Path):
-        return Image.open(image_file).convert("RGB")
+    def load_image_from_file(cls, image_file: Path) -> np.ndarray:
+        """Loads images as np.array using opencV
+
+        Args:
+            image_file (Path): path to image file
+
+        Returns:
+            np.ndarray: numpy array image
+        """
+        if cls.image_type == np.ndarray:
+            return _load_image_usin_openCV(image_file)
+        elif cls.image_type == Image:
+            return _load_image_using_PIL(image_file)
 
     def copy(self, facial_part: FacialPart = FacialParts.entire):
         return Dataset300W(self.meta["images_dir_path"], facial_part)
