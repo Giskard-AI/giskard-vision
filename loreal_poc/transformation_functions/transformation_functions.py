d95af6b7b084d5da3577ffd00c49cd2004f87d2e
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-12-06 17:20:46 +0100

    refactoring of transformation functions

--- loreal_poc/transformation_functions/transformation_functions.py
+++ loreal_poc/transformation_functions/transformation_functions.py
@@ -3,23 +3,23 @@ from ..datasets.base import DatasetBase
 from ..datasets.base import FacialPart
 
 
-def crop_based_on_facial_part(ds: DatasetBase, facial_part: FacialPart, rel_margin: float = 0.0):
+def crop_based_on_facial_part(dataset: DatasetBase, facial_part: FacialPart, rel_margin: float = 0.0):
     """Crop image based on marks
 
     Args:
-        image (Image): The Image to crop
-        marks (np.ndarray): The marks to base the cropping on
+        dataset (DatasetBase): dataset to crop
+        facial_part (FacialPart): The facial_part to base the cropping on
         rel_margin (float, optional): Margins to add on top of the limit founds from the marks. Defaults to 0.0.
 
     Returns:
         _type_: image
     """
-    cropped_ds = ds.copy()
-    for i, image in enumerate(cropped_ds.all_images):
+    _dataset = dataset.copy()
+    for i, image in enumerate(_dataset.all_images):
         w, h = image.size
         margins = np.array([w, h]) * rel_margin
 
-        marks = ds.marks_for(facial_part, i)
+        marks = _dataset.marks_for(facial_part, i)
         L, R = np.nanmin(marks, axis=0), np.nanmax(marks, axis=0)
 
         left = L[0] - margins[0]
@@ -27,6 +27,6 @@ def crop_based_on_facial_part(ds: DatasetBase, facial_part: FacialPart, rel_marg
         right = R[0] + margins[0]
         lower = R[1] + margins[1]
 
-        cropped_ds._all_images[i] = image.crop((left, upper, right, lower))  # TODO: OpenCV
+        _dataset._all_images[i] = image.crop((left, upper, right, lower))  # TODO: OpenCV
 
-    return cropped_ds
+    return _dataset
