5effb4cf1f6bb4a789b0ffc9c509a9e174f3971a
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-12-07 00:38:20 +0100

    removed PIL support

--- loreal_poc/transformation_functions/transformation_functions.py
+++ loreal_poc/transformation_functions/transformation_functions.py
@@ -4,8 +4,18 @@ from ..datasets.base import DatasetBase
 from ..datasets.base import FacialPart
 
 
+def get_boundaries_from_marks(marks, margins):
+    L, R = np.nanmin(marks, axis=0), np.nanmax(marks, axis=0)
+
+    left = int(L[0] - margins[0])
+    lower = int(L[1] - margins[1])
+    right = int(R[0] + margins[0])
+    upper = int(R[1] + margins[1])
+    return left, upper, right, lower
+
+
 def crop_based_on_facial_part(dataset: DatasetBase, facial_part: FacialPart, rel_margin: float = 0.0):
-    """Crop image based on marks
+    """Crop image based on marks using openCV
 
     Args:
         dataset (DatasetBase): dataset to crop
@@ -17,17 +27,17 @@ def crop_based_on_facial_part(dataset: DatasetBase, facial_part: FacialPart, rel
     """
     _dataset = dataset.copy(facial_part)
     for i, image in enumerate(_dataset.all_images):
-        w, h = image.size
+        h, w, _ = image.shape
         margins = np.array([w, h]) * rel_margin
 
         marks = _dataset.marks_for(facial_part, i)
-        L, R = np.nanmin(marks, axis=0), np.nanmax(marks, axis=0)
+        left, upper, right, lower = get_boundaries_from_marks(marks, margins)
 
-        left = L[0] - margins[0]
-        upper = L[1] - margins[1]
-        right = R[0] + margins[0]
-        lower = R[1] + margins[1]
+        image[:lower, :] = np.array([0, 0, 0])
+        image[:, right:] = np.array([0, 0, 0])
+        image[upper:, :] = np.array([0, 0, 0])
+        image[:, :left] = np.array([0, 0, 0])
 
-        _dataset._all_images[i] = image.crop((left, upper, right, lower))  # TODO: OpenCV
+        _dataset._all_images[i] = image
 
     return _dataset
