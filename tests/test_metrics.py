cd8dd5be88337b813488d7b2bdf05d6e4db88506
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-12-09 14:50:48 +0100

    fixed conflicts with main

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,12 +1,13 @@
 import numpy as np
+
+from loreal_poc.models.base import PredictionResult
 from loreal_poc.tests.performance import (
-    _calculate_es,
-    _calculate_nmes,
-    _calculate_d_outers,
     LEFT_EYE_LEFT_LANDMARK,
     RIGHT_EYE_RIGHT_LANDMARK,
+    Es,
+    NMEs,
+    _calculate_d_outers,
 )
-
 
 TEST_ARRAY_A_2D = [[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [1.0, 1.0]]
 TEST_ARRAY_B_2D = [[4.0, 0.0], [1.0, 1.0], [2.0, 2.0], [2.0, 3.0]]
@@ -159,7 +160,8 @@ def test_calculate_es_2d():
     a = np.asarray(TEST_ARRAY_A_2D)
     b = np.asarray(TEST_ARRAY_B_2D)
     c = np.asarray([np.sqrt((p_a[0] - p_b[0]) ** 2 + (p_a[1] - p_b[1]) ** 2) for p_a, p_b in zip(a, b)])
-    calculated = _calculate_es(np.asarray([a]), np.asarray([b]))
+    prediction_result = PredictionResult(prediction=a)
+    calculated = Es.definition(prediction_result, np.asarray([b]))
     assert np.all(np.isclose(np.asarray([c]), calculated))
 
 
@@ -169,7 +171,8 @@ def test_calculate_es_3d():
     c = np.asarray(
         [np.sqrt((p_a[0] - p_b[0]) ** 2 + (p_a[1] - p_b[1]) ** 2 + (p_a[2] - p_b[2]) ** 2) for p_a, p_b in zip(a, b)]
     )
-    calculated = _calculate_es(np.asarray([a]), np.asarray([b]))
+    prediction_result = PredictionResult(prediction=np.asarray([a]))
+    calculated = Es.get(prediction_result, np.asarray([b]))
     assert np.all(np.isclose(np.asarray([c]), calculated))
 
 
@@ -190,8 +193,8 @@ def test_calculate_d_outers():
 
 def test_calculate_nmes():
     marks = np.asarray([TEST_MARKS])
-    predictions = np.asarray([PREDICT_MARKS])
-    calculated = _calculate_nmes(predictions, marks)
+    prediction_result = PredictionResult(prediction=np.asarray([PREDICT_MARKS]))
+    calculated = NMEs.get(prediction_result, marks)
 
     distances = np.asarray(
         [
@@ -204,7 +207,10 @@ def test_calculate_nmes():
     )
     me = np.mean(
         np.asarray(
-            [np.sqrt((p_a[0] - p_b[0]) ** 2 + (p_a[1] - p_b[1]) ** 2) for p_a, p_b in zip(predictions[0], marks[0])]
+            [
+                np.sqrt((p_a[0] - p_b[0]) ** 2 + (p_a[1] - p_b[1]) ** 2)
+                for p_a, p_b in zip(prediction_result.prediction[0], marks[0])
+            ]
         )
     )
 
