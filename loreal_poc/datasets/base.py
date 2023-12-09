67d66876d969be65615f78f367439435a5c3eb92
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-12-09 12:13:42 +0100

    Improved FacialPart ops

--- loreal_poc/datasets/base.py
+++ loreal_poc/datasets/base.py
@@ -14,7 +14,10 @@ class FacialPart(NDArrayOperatorsMixin):
     name: str = ""
 
     def __add__(self, o):
-        return FacialPart(np.concatenate((self.part, o.part)))
+        return FacialPart(np.intersect1d((self.part, o.part)))
+
+    def __sub__(self, o):
+        return FacialPart(np.setxor1d((self.part, o.part)))
 
     def __array__(self):
         return self.part
@@ -158,3 +161,6 @@ class DatasetBase(ABC):
         part_landmarks = self.all_marks[mark_idx].copy()
         part_landmarks[idx] = np.nan
         return part_landmarks
+
+
+# %%
