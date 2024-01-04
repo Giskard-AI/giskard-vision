31388f3ca459288128ade557d53abd509e111804
Pierre Le Jeune <pierre@giskard.ai>
2024-01-04 16:57:11 +0100

    Add a function to compute scale_ratios outside the resize function

--- loreal_poc/transformation_functions/resize.py
+++ loreal_poc/transformation_functions/resize.py
@@ -4,20 +4,8 @@ import cv2
 import numpy as np
 
 
-def resize_image_with_marks(
-    img: np.ndarray, marks: np.ndarray | None, scales: Union[Tuple[float, float], float], absolute_scales: bool
-) -> np.ndarray:
-    resized_img, scale_ratios = resize_image(img, scales, absolute_scales, return_ratios=True)
-    if marks is not None:
-        resized_marks = resize_marks(marks, scale_ratios)
-        return resized_img, resized_marks
-    return resized_img
-
-
-def resize_image(
-    img: np.ndarray, scales: Union[Tuple[float, float], float], absolute_scales: bool, return_ratios: bool = False
-) -> np.ndarray:
-    h, w, _ = img.shape
+def compute_scale_ratios(scales: Union[Tuple[float, float], float], absolute_scales: bool, img_shape: tuple) -> tuple:
+    h, w = img_shape
     if absolute_scales:
         if isinstance(scales, tuple):
             scale_ratios = (scales[0] / w, scales[1] / h)
@@ -32,6 +20,25 @@ def resize_image(
 
     if any([s <= 0 for s in scale_ratios]):
         raise ValueError(f"Invalid scale parameter: {scale_ratios} contains at least one non-positive scale ratio.")
+
+    return scale_ratios
+
+
+def resize_image_with_marks(
+    img: np.ndarray, marks: np.ndarray | None, scales: Union[Tuple[float, float], float], absolute_scales: bool
+) -> np.ndarray:
+    resized_img, scale_ratios = resize_image(img, scales, absolute_scales, return_ratios=True)
+    if marks is not None:
+        resized_marks = resize_marks(marks, scale_ratios)
+        return resized_img, resized_marks
+    return resized_img
+
+
+def resize_image(
+    img: np.ndarray, scales: Union[Tuple[float, float], float], absolute_scales: bool, return_ratios: bool = False
+) -> np.ndarray:
+    h, w, _ = img.shape
+    scale_ratios = compute_scale_ratios(scales, absolute_scales, (h, w))
 
     resized_width = int(w * scale_ratios[0])
     resized_height = int(h * scale_ratios[1])
