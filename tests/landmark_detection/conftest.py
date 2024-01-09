26e9f9ce0eb31bd92e2c5769a99df311b7ae3177
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2024-01-09 19:32:56 +0100

    cleanup

--- tests/landmark_detection/conftest.py
+++
@@ -1,7 +0,0 @@
-from glob import glob
-from pathlib import Path
-
-pytest_plugins = []
-for f in glob("**/fixtures/**/*.py", recursive=True):
-    path = Path(f)
-    pytest_plugins.append(".".join([*path.parts[:-1], path.stem]))
