8118120ac1619d55d75423e5c1e0e82a0b4735af
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2024-01-12 19:08:44 +0100

    improved GiskardImportError

--- giskard_vision/landmark_detection/utils/errors.py
+++ giskard_vision/landmark_detection/utils/errors.py
@@ -1,4 +1,12 @@
+from typing import Union, List
+
 class GiskardImportError(ImportError):
-    def __init__(self, missing_package: str) -> None:
+    def __init__(self, missing_packages: Union[List[str], str]) -> None:
         super().__init__()
-        self.msg = f"The '{missing_package}' Python package is not installed; please execute 'pip install {missing_package}' to obtain it."
+        
+        if isinstance(missing_packages, list):
+            self.msg = f"The '{missing_packages}' Python packages are not installed; please execute 'pip install {" ".join(missing_packages)}' to obtain it."
+        elif isinstance(missing_packages, str):
+            self.msg = f"The '{missing_packages}' Python package is not installed; please execute 'pip install {missing_packages}' to obtain it."
+        else:
+            raise ValueError(f"{self.__class__.__name__}: takes only a list of strings, or a single string, instead {type(missing_packages)} was given")
