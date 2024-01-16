a0ced051291aa46d1c8a811a578b4d581cdff996
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2024-01-16 15:11:02 +0100

    modified behaviour of tests w.r.t. cropped DL

---
+++ tests/landmark_detection/tests_and_metrics/test_tests.py
@@ -0,0 +1,27 @@
+from giskard_vision.landmark_detection.dataloaders.wrappers import CroppedDataLoader
+
+from giskard_vision.landmark_detection.tests.performance import NMEMean
+from giskard_vision.landmark_detection.tests.base import Test, TestDiff
+from giskard_vision.landmark_detection.marks.facial_parts import FacialParts
+
+
+def test_tests_on_cropped_dl(opencv_model, dataset_300w):
+    
+    fp = FacialParts.LEFT_HALF.value
+    dl = CroppedDataLoader(dataset_300w, part=fp)
+    
+    for test in [Test, TestDiff]:
+        kwargs = {
+            "model": opencv_model,
+            "dataloader": dl,
+            "facial_part": fp 
+        }
+        if test == TestDiff:
+            kwargs["dataloader_ref"] = dataset_300w
+
+        test1 = test(metric=NMEMean, threshold=1).run(**kwargs)
+        kwargs.pop("facial_part")
+        test2 = test(metric=NMEMean, threshold=1).run(**kwargs)
+    
+        assert test1.metric_value == test2.metric_value
+                
