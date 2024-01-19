63eb6661953f7e8722f43826886f45e8e4f8d0d7
Benoît Malézieux <benoit.mx2010@gmail.com>
2024-01-19 15:43:23 +0100

    FIX: safe import in test

--- giskard_vision/__init__.py
+++ giskard_vision/__init__.py
@@ -1,5 +1,9 @@
-from giskard_vision.landmark_detection import detectors
+try:
+    from giskard_vision.landmark_detection import detectors
 
-from . import landmark_detection
+    from . import landmark_detection
 
-__all__ = ["landmark_detection", "detectors"]
+    __all__ = ["landmark_detection", "detectors"]
+except (ImportError, ModuleNotFoundError):
+    print("Please install giskard to use custom detectors")
+    __all__ = ["landmark_detection"]
--- tests/landmark_detection/detectors/test_detectors.py
+++ tests/landmark_detection/detectors/test_detectors.py
@@ -1,42 +1,40 @@
-from giskard_vision.landmark_detection.detectors import (
-    CroppingDetectorLandmark,
-    EthnicityDetectorLandmark,
-    HeadPoseDetectorLandmark,
-    TransformationBlurringDetectorLandmark,
-    TransformationColorDetectorLandmark,
-    TransformationResizeDetectorLandmark,
-)
-from giskard_vision.landmark_detection.detectors.base import ScanResult
-
 try:
     from giskard.scanner.issues import Issue, IssueLevel
-except (ImportError, ModuleNotFoundError) as e:
-    e.msg = "Please install giskard to use custom detectors"
-    raise e
+    from pytest import mark
 
-from pytest import mark
-
-
-@mark.parametrize(
-    "detector",
-    [
+    from giskard_vision.landmark_detection.detectors import (
         CroppingDetectorLandmark,
-        HeadPoseDetectorLandmark,
         EthnicityDetectorLandmark,
+        HeadPoseDetectorLandmark,
         TransformationBlurringDetectorLandmark,
         TransformationColorDetectorLandmark,
         TransformationResizeDetectorLandmark,
-    ],
-)
-def test_base_detector(opencv_model, dataset_300w, detector):
-    results = detector().get_results(opencv_model, dataset_300w)
-    assert isinstance(results, list)
-    assert len(results) > 0
-    assert isinstance(results[0], ScanResult)
-
-    issues = detector().get_issues(
-        opencv_model, dataset_300w, results, (IssueLevel.MINOR, IssueLevel.MEDIUM, IssueLevel.MAJOR)
     )
-    assert isinstance(results, list)
-    assert len(issues) > 0
-    assert isinstance(issues[0], Issue)
+    from giskard_vision.landmark_detection.detectors.base import ScanResult
+
+    @mark.parametrize(
+        "detector",
+        [
+            CroppingDetectorLandmark,
+            HeadPoseDetectorLandmark,
+            EthnicityDetectorLandmark,
+            TransformationBlurringDetectorLandmark,
+            TransformationColorDetectorLandmark,
+            TransformationResizeDetectorLandmark,
+        ],
+    )
+    def test_base_detector(opencv_model, dataset_300w, detector):
+        results = detector().get_results(opencv_model, dataset_300w)
+        assert isinstance(results, list)
+        assert len(results) > 0
+        assert isinstance(results[0], ScanResult)
+
+        issues = detector().get_issues(
+            opencv_model, dataset_300w, results, (IssueLevel.MINOR, IssueLevel.MEDIUM, IssueLevel.MAJOR)
+        )
+        assert isinstance(results, list)
+        assert len(issues) > 0
+        assert isinstance(issues[0], Issue)
+
+except (ImportError, ModuleNotFoundError):
+    print("Please install giskard to use custom detectors")
