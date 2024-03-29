e81af840f72b54ec561e3baeea077ed4a8b9616a
Rabah Abdul Khalek <rabahabdulkhalek@Rabahs-MacBook-Pro-2.local>
2024-03-29 19:02:11 +0100

    fixing report.py to_json

--- giskard_vision/landmark_detection/tests/report.py
+++ giskard_vision/landmark_detection/tests/report.py
@@ -152,8 +152,7 @@ class Report:
             from datetime import datetime
 
             current_time = str(datetime.now()).replace(" ", "-")
-
-        filename = f"report_{current_time}.md"
+            filename = f"report_{current_time}.md"
 
         with open(filename, "w") as jsonl_file:
             for result in self.results:
