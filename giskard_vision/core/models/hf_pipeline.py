48c17ac4ab75656a2c5a7fd0724e6fcbb640b490
Weixuan Xiao <weixuan@giskard.ai>
2024-07-18 17:56:47 +0200

    Add base model for Hugging Face pipeline

---
+++ giskard_vision/core/models/hf_pipeline.py
@@ -0,0 +1,46 @@
+from enum import Enum
+from typing import Any, Optional
+
+import numpy as np
+
+from giskard_vision.core.models.base import ModelBase
+from giskard_vision.utils.errors import GiskardImportError
+
+
+class HuggingFacePipelineTask(Enum):
+    IMAGE_CLASSIFICATION = "image-classification"
+    OBJECT_DETECTION = "object-detection"
+
+
+class HuggingFacePipelineModelBase(ModelBase):
+    """Abstract class that serves as a template for model predictions based on HuggingFace pipelines"""
+
+    def __init__(
+        self, model_id: str, pipeline_task: HuggingFacePipelineTask, name: Optional[str] = None, device: str = "cpu"
+    ) -> None:
+        """init method that accepts a model object, number of landmarks and dimensions
+
+        Args:
+            model_id (str): Hugging Face model ID
+            name (Optional[str]): name of the model
+            pipeline_task (HuggingFacePipelineTask): HuggingFace pipeline task
+
+        Raises:
+            GiskardImportError: If there are missing Hugging Face dependencies.
+        """
+        self.name = name
+        self.pipeline_task = pipeline_task
+        try:
+            from transformers import pipeline
+
+            self.pipeline = pipeline(task=pipeline_task.value, model=model_id, device=device)
+        except ImportError as e:
+            raise GiskardImportError(["transformers"]) from e
+
+    def predict_image(self, image: np.ndarray) -> Any:
+        """abstract method that takes one image as input and outputs the prediction
+
+        Args:
+            image (np.ndarray): input image
+        """
+        return self.pipeline(image)
