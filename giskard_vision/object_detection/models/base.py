949144be4c08882d93e1953b7a5cf40dd1ff5a05
Weixuan Xiao <weixuan@giskard.ai>
2024-07-26 18:46:00 +0200

    Merge with `face-detection-scan` to make scan work

--- giskard_vision/object_detection/models/base.py
+++ giskard_vision/object_detection/models/base.py
@@ -1,6 +1,7 @@
 from typing import Any, Optional
 
 import numpy as np
+from PIL import Image
 
 from giskard_vision.core.models.hf_pipeline import HFPipelineModelBase, HFPipelineTask
 from giskard_vision.object_detection.types import Types
@@ -8,19 +9,17 @@ from giskard_vision.object_detection.types import Types
 
 class ObjectDetectionHFModel(HFPipelineModelBase):
     """Hugging Face pipeline wrapper class that serves as a template for image classification predictions
-
     Args:
         model_id (str): Hugging Face model ID
         name (Optional[str]): name of the model
         device (str): device to run the model on
     """
 
-    model_type = "object-detection"
+    model_type = "object_detection"
     prediction_result_cls = Types.prediction_result
 
     def __init__(self, model_id: str, name: Optional[str] = None, device: str = "cpu"):
         """init method that accepts a model id, name and device
-
         Args:
             model_id (str): Hugging Face model ID
             name (Optional[str]): name of the model
@@ -36,8 +35,7 @@ class ObjectDetectionHFModel(HFPipelineModelBase):
 
     def predict_raw(self, image: np.ndarray) -> Any:
         """method that takes one image as input and outputs the raw predictions
-
         Args:
             image (np.ndarray): input image
         """
-        return self.pipeline(image)
+        return self.pipeline(Image.fromarray(image, "RGB"))
--- giskard_vision/object_detection/models/wrappers.py
+++ giskard_vision/object_detection/models/wrappers.py
@@ -1,6 +1,9 @@
-from typing import Any
+import os
+from typing import Any, Optional
 
+import cv2
 import numpy as np
+import pandas as pd
 
 from giskard_vision.core.models.base import ModelBase
 from giskard_vision.object_detection.models.base import ObjectDetectionHFModel
@@ -209,9 +212,153 @@ class FasterRCNNWheat(FasterRCNNBase):
         return image
 
 
+class RacoonDetection(ModelBase):
+    model_weights: str = "racoon_detection.h5"
+    model_type: str = "object_detection"
+    image_size: int = 128
+    alpha: float = 1.0
+
+    def __init__(self, data_path: Optional[str] = None) -> None:
+        self.model = self.get_model_arch()
+
+        if os.path.exists(self.model_weights):
+            self.model.load_weights(self.model_weights)
+        else:
+            if not data_path:
+                raise ValueError("data_path is required for training the model")
+            self.train(data_path)
+            self.model.save(self.model_weights)
+
+    def get_model_arch(self):
+        try:
+            from keras import Model
+            from keras.applications.mobilenet import MobileNet
+            from keras.layers import Conv2D, Reshape
+        except ImportError:
+            raise GiskardImportError(["keras", "tensorflow"])
+
+        model = MobileNet(input_shape=(self.image_size, self.image_size, 3), include_top=False, alpha=self.alpha)
+
+        for layers in model.layers:
+            layers.trainable = False
+
+        x = model.layers[-1].output
+        x = Conv2D(4, kernel_size=4, name="coords")(x)
+        x = Reshape((4,))(x)
+
+        return Model(inputs=model.inputs, outputs=x)
+
+    @staticmethod
+    def loss(gt, pred):
+        try:
+            from keras.backend import epsilon
+        except ImportError:
+            raise GiskardImportError(["keras"])
+
+        intersections = 0
+        unions = 0
+        diff_width = np.minimum(gt[:, 0] + gt[:, 2], pred[:, 0] + pred[:, 2]) - np.maximum(gt[:, 0], pred[:, 0])
+        diff_height = np.minimum(gt[:, 1] + gt[:, 3], pred[:, 1] + pred[:, 3]) - np.maximum(gt[:, 1], pred[:, 1])
+        intersection = diff_width * diff_height
+
+        # Compute union
+        area_gt = gt[:, 2] * gt[:, 3]
+        area_pred = pred[:, 2] * pred[:, 3]
+        union = area_gt + area_pred - intersection
+
+        #     Compute intersection and union over multiple boxes
+        for j, _ in enumerate(union):
+            if union[j] > 0 and intersection[j] > 0 and union[j] >= intersection[j]:
+                intersections += intersection[j]
+                unions += union[j]
+
+        # Compute IOU. Use epsilon to prevent division by zero
+        iou = np.round(intersections / (unions + epsilon()), 4)
+        iou = iou.astype(np.float32)
+        return iou
+
+    @staticmethod
+    def IoU(y_true, y_pred):
+        try:
+            import tensorflow as tf
+        except ImportError:
+            raise GiskardImportError(["tensorflow"])
+        iou = tf.py_function(RacoonDetection.loss, [y_true, y_pred], tf.float32)
+        return iou
+
+    def train(self, data_path):
+        try:
+            from keras.applications.mobilenet import preprocess_input
+            from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+            from PIL import Image
+
+        except ImportError:
+            raise GiskardImportError(["PIL", "keras"])
+
+        train = pd.read_csv(data_path + "/train_labels_.csv")
+
+        paths = train["filename"]
+
+        coords = train[["width", "height", "xmin", "ymin", "xmax", "ymax"]]
+
+        coords["xmin"] = coords["xmin"] * self.image_size / coords["width"]
+        coords["xmax"] = coords["xmax"] * self.image_size / coords["width"]
+        coords["ymin"] = coords["ymin"] * self.image_size / coords["height"]
+        coords["ymax"] = coords["ymax"] * self.image_size / coords["height"]
+
+        coords.drop(["width", "height"], axis=1, inplace=True)
+        coords.head()
+
+        images = data_path + "/Racoon Images/images/"
+
+        batch_images = np.zeros((len(paths), self.image_size, self.image_size, 3), dtype=np.float32)
+
+        for i, f in enumerate(paths):
+            img = Image.open(images + f)
+            img = img.resize((self.image_size, self.image_size))
+            img = img.convert("RGB")
+            batch_images[i] = preprocess_input(np.array(img, dtype=np.float32))
+
+        gt = coords
+
+        PATIENCE = 10
+
+        self.model.compile(optimizer="Adam", loss="mse", metrics=[self.IoU])
+
+        stop = EarlyStopping(monitor="IoU", patience=PATIENCE, mode="max")
+
+        reduce_lr = ReduceLROnPlateau(monitor="IoU", factor=0.2, patience=PATIENCE, min_lr=1e-7, verbose=1, mode="max")
+
+        self.model.fit(batch_images, gt, epochs=100, callbacks=[stop, reduce_lr], verbose=2)
+
+    def shape_rescale(self, image, boxes):
+        image_height, image_width, _ = image.shape
+
+        x0 = int(boxes[0] * image_width / self.image_size)
+        y0 = int(boxes[1] * image_height / self.image_size)
+
+        x1 = int((boxes[2]) * image_width / self.image_size)
+        y1 = int((boxes[3]) * image_height / self.image_size)
+
+        return np.array([x0, y0, x1, y1])
+
+    def positive_constraint(self, boxes):
+        np.clip(boxes, 0, None)
+
+    def predict_image(self, image: np.ndarray):
+        try:
+            from keras.applications.mobilenet import preprocess_input
+        except ImportError:
+            raise GiskardImportError(["keras"])
+
+        resized_image = cv2.resize(image, (self.image_size, self.image_size))
+        feat_scaled = preprocess_input(np.array(resized_image, dtype=np.float32))
+        boxes = self.model.predict(x=np.array([feat_scaled]))[0]
+        return {"boxes": self.positive_constraint(self.shape_rescale(image, boxes)), "labels": "racoon"}
+
+
 class DetrFinetunedFaceDetectionHuggingFaceModel(ObjectDetectionHFModel):
     """Wrapper class for goshiv's detr finetuned face detection model on Hugging Face.
-
     Args:
         name (str): The name of the model.
         device (str): The device to run the model on.
@@ -230,11 +377,14 @@ class DetrFinetunedFaceDetectionHuggingFaceModel(ObjectDetectionHFModel):
         # Filter out predictions with a highest score
         best_prediction = max(raw_predictions, key=lambda x: x["score"])
 
-        return np.array(
-            [
-                best_prediction["box"]["xmin"],
-                best_prediction["box"]["ymin"],
-                best_prediction["box"]["xmax"],
-                best_prediction["box"]["ymax"],
-            ]
-        )
+        return {
+            "boxes": np.array(
+                [
+                    best_prediction["box"]["xmin"],
+                    best_prediction["box"]["ymin"],
+                    best_prediction["box"]["xmax"],
+                    best_prediction["box"]["ymax"],
+                ]
+            ),
+            "labels": "face",
+        }
