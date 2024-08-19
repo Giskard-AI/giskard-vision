1b2d56d8331659fa94130818cbc9c6ef46e418b8
Rabah Khalek <rabah.khalek@gmail.com>
2024-08-19 13:32:38 +0200

    added core to init

--- giskard_vision/__init__.py
+++ giskard_vision/__init__.py
@@ -1,4 +1,4 @@
-from . import image_classification, landmark_detection, object_detection
+from . import core, image_classification, landmark_detection, object_detection
 from .core.scanner import scan
 
-__all__ = ["scan", "landmark_detection", "image_classification", "object_detection"]
+__all__ = ["scan", "core", "landmark_detection", "image_classification", "object_detection"]
